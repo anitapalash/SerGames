f94a09a8ebf081c902912cb294dc593d6b55ee54
anitapalash <647316392@github.com>
2018-10-11 20:10:49 +0300

    output function

--- src/word_game/Mech.java
+++ src/word_game/Mech.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 public class Mech {
+    private static String base;
     public static void main(String[] args) throws IOException {
         //создаем поток чтения из файла
        String fileName = "C:\\Users\\Анна\\Desktop\\word.txt";
@@ -20,8 +22,8 @@ public class Mech {
        ArrayList<String> words = new ArrayList<>();
 
        //запрашиваем базовое слово
-       System.out.println("Enter base word:");
-       String base = input.readLine();
+       System.out.println("Введите базовое слово:");
+       base = input.readLine();
        List<Character> baseList;
 
        //считываем слова из файла до его конца
@@ -36,12 +38,7 @@ public class Mech {
        }
 
        //вывод результатов
-       System.out.println("All possible words:");
-       if (words.size() == 0)
-           System.out.println("None.");
-       else
-           for (String s : words)
-               System.out.println(s);
+        output(words);
 
         //закрываем потоки
        reader.close();
@@ -51,7 +48,7 @@ public class Mech {
     //функция для обработки слова
     private static List<Character> sortList(String word) {
         //все слова переводим в нижний регистр
-        word.toLowerCase();
+        word = word.toLowerCase();
         //превращаем слово в массив и сортируем по алфавиту
         char[] wordArr = word.toCharArray();
         Arrays.sort(wordArr);
@@ -76,5 +73,33 @@ public class Mech {
         }
 
         return result;
+    }
+
+    //функция для вывода
+    private static void output(List<String> list) {
+        //вывод в случае пустого списка
+        if (list.size() == 0) {
+            System.out.println("Слов нет");
+            return;
+        }
+
+        //сортируем по алфавиту
+        Collections.sort(list);
+
+        //найдем максимальную длину слова из всего списка
+        int maxLength = 2;
+        for (String s : list)
+            if (s.length() > maxLength)
+                maxLength = s.length();
+
+        //вывод
+        for (int i = 2; i < maxLength; i++) {
+            System.out.println("Слова из " + i + " букв, составленные из слова \"" + base +  "\":");
+            for (String word : list) {
+                if (word.length() == i)
+                    System.out.print(word + " ");
+            }
+            System.out.println("\n");
+        }
     }
 }
