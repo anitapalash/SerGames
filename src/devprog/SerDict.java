b5433203fac7d6c166013b047e7d2579b8c53b28
anitapalash <647316392@github.com>
2018-11-29 12:03:30 +0300

    added addWord function

--- src/devprog/SerDict.java
+++ src/devprog/SerDict.java
@@ -9,31 +9,32 @@ public class SerDict {
     static ArrayList<String> dictionary = new ArrayList<>();
     //путь к словарю
     static String path = "C:\\Users\\Анна\\IdeaProjects\\Serg1\\src\\word_game\\dictionaries\\singular.txt";
+    static boolean flag = false;        //флаг того, что были какие-либо изменения
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Please, wait a minute...");
+        System.out.println("Подожди минуту, пожалуйста...");
         DictionaryMaker maker = new DictionaryMaker();
         maker.run();
 
         while (true) {
-            System.out.println("Choose function: (enter a number of it)");
-            System.out.println("1. Check if dictionary has this word or not");
-            System.out.println("2. Fuse files");
-            System.out.println("3. Exit");
+            System.out.println("Выбери функцию: (введи её номер)");
+            System.out.println("1. Проверить наличие слова в словаре");
+            System.out.println("2. Соединить файлы");
+            System.out.println("3. Выход");
             int choice;
 
             try {
                 choice = Integer.parseInt(reader.readLine());
             } catch (NumberFormatException e) {
-                System.out.println("Something gone wrong. Try one more time");
+                System.out.println("Что-то пошло не так. Попробуй ещё раз");
                 continue;
             }catch (IOException e) {
-                System.out.println("Something gone wrong. Try one more time");
+                System.out.println("Что-то пошло не так. Попробуй ещё раз");
                 continue;
             }
 
             if(choice == 3) {
-                System.out.println("Bye!");
+                System.out.println("Пока!");
                 break;
             }
 
@@ -47,19 +48,33 @@ public class SerDict {
                     default:
                         continue;
             }
-
-            Collections.sort(dictionary);
-            FileWriter fw = new FileWriter(path,false);
-            for (String s : dictionary) {
-                fw.write(s);
-                fw.write("\n");
-            }
+            if (flag)
+                maker.end();
         }
     }
 
     //функция для ввода слова и добавления его в случае необходимости
-    public static void addWord() {
-
+    public static void addWord() throws IOException {
+        while (true) {
+            System.out.println("\nВведи слово:");
+            String word = reader.readLine();
+            if (dictionary.contains(word))
+                System.out.println("Такое уже есть");
+            else {
+                System.out.println("Да, такого нет, хочешь добавить? (д/н/e)");
+                String choice = reader.readLine();
+                if (choice.equals("д")) {
+                    dictionary.add(word);
+                    flag = true;
+                }
+                else if (choice.equals("н"))
+                    System.out.println("Окей, тогда продолжим");
+                else {
+                    System.out.println("Я верну тебя в главное меню");
+                    return;
+                }
+            }
+        }
     }
 
     public static void fuse() {
@@ -78,6 +93,22 @@ public class SerDict {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        public void end() throws IOException {
+            Collections.sort(dictionary);
+            //чистим файл
+            PrintWriter printWriter = new PrintWriter(path);
+            printWriter.print("");
+            printWriter.close();
+
+            //пишем в файл наш обновленный словарь
+            BufferedWriter fw = new BufferedWriter(new FileWriter(new File(path)));
+            for (String s : dictionary) {
+                fw.write(s);
+                fw.newLine();
+            }
+            fw.close();
         }
     }
 }
